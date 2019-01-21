5dc9992124eace210654cf7547674c499b3f3970
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-01-21 10:02:33 -0500

    Add first pass of Spotify functionality.

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const nodeSpotify = require("node-spotify-api");
 const moment = require("moment");
 const inquirer = require("inquirer");
-const spotify = keys.spotify;
+const spotify = new nodeSpotify({ id: keys.spotify.id, secret: keys.spotify.secret });
 const omdb = keys.omdb;
 const bandsintown = keys.bandsintown;
 
@@ -80,10 +80,35 @@ inquirer.prompt([
 
     switch(response.command) {
 
+        // Spotify API call.
         case "spotify": 
-            console.log('spotify called');
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "spotifyArg",
+                    message: "What song do you want to learn more about?",
+                    validate: validateThis
+                }
+            ]).then(response => {
+                const song = capitalize(response.spotifyArg);
+                spotify.search({ type: 'track', query: song }, function(error, data) {
+                    if (error) { return console.log(`There was an error with that song: ${error}`) };
+
+                    if (data === "undefined") { return console.log("No songs found. Try again!") };
+
+                    const result = `\nArtist: ${data.tracks.items[0].artists[0].name}\nTrack: ${data.tracks.items[0].name}\nAlbum: ${data.tracks.items[0].album.name}\nPreview Track: ${data.tracks.items[0].preview_url}\n`;
+
+                    console.log(result);
+                    print((result + "\n############\n"));
+
+                });
+            
+            }).catch(error => {
+                console.log(`Hmmm, something went wrong (${error}). Try again!`);
+            });
             break;
 
+        // OMDB API call.
         case "omdb":
             inquirer.prompt([
                 {
@@ -106,10 +131,10 @@ inquirer.prompt([
                         if (response.data.Response === "False") { return console.log(`${movie} doesn't appear to be a movie. Try again!`) };
 
                         // Otherwise compile result, display in console, and append to log.txt
-                        let result = `\n############\nTitle: ${response.data.Title}\nReleased: ${response.data.Released}\nIMDB Rating: ${response.data.Ratings[0].Value}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nCast: ${response.data.Actors}\n############\n`;
+                        let result = `\nTitle: ${response.data.Title}\nReleased: ${response.data.Released}\nIMDB Rating: ${response.data.Ratings[0].Value}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nCast: ${response.data.Actors}\n`;
 
                         console.log(result);
-                        print(result);
+                        print((result + "\n############\n"));
 
                     })
                     .catch(error => {
@@ -120,6 +145,7 @@ inquirer.prompt([
             });
             break;
 
+        // Bands in Town API call.
         case "bandsintown":
             inquirer.prompt([
                 {
@@ -148,7 +174,7 @@ inquirer.prompt([
                             result += `\nArtist: ${artist}\nVenue: ${events[i].venue.name}\nLocation: ${events[i].venue.city}, ${events[i].venue.region}, ${events[i].venue.country}\nDate: ${moment(events[i].datetime).format('MMMM Do YYYY')}\n\n`;
                         }
                         console.log(result);
-                        print((result + "############"));
+                        print((result + "\n############\n"));
 
                     })
                     .catch(error => {
@@ -162,10 +188,12 @@ inquirer.prompt([
             });
             break;
 
+        // Surprise me with one of the above API calls.
         case "surprise":
             console.log("surprise");
             break;
 
+        // Default resolves to a note about trying again.
         default:
             return console.log("Hmmm, something went wrong. Try again");
             
