d2fd2bf229cfbbcef66844720df8819404667c8f
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-01-26 16:08:27 -0500

    Parcel helper functions into seperate file. Add module imports for liri.js

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require("dotenv").config();
+const helpers = require("./helpers"); // This module contains our helper functions.
 const keys = require("./keys.js");
 const fs = require("fs");
 const axios = require("axios");
@@ -8,60 +9,13 @@ const nodeSpotify = require("node-spotify-api");
 const moment = require("moment");
 const inquirer = require("inquirer");
 
-// Function that validates our Inquirer entries.
-function validateThis(value) {
-
-    var pass = value.match(
-        /([A-Za-z0-9-]+)/i
-    );
-    
-    if (pass) { return true; }
-    return "Please enter a valid entry."
-
-}
-
-// Function that logs our outputs to a text file.
-function print(result) {
-
-    fs.appendFile("log.txt", result, function(err) {
-        if (err) { return console.log(`Error writing to log: ${err}`) }
-    });
-
-}
-
-// Function that concatenates our API inputs so there aren't blank spaces.
-function concat(input) {
-
-    return input.trim().split(" ").join("+");
-
-}
-
-// Function that capitalizes user inputs.
-function capitalize(string) {
-
-    if (!string.includes(" ")) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-
-    let capitalized = [];
-    let arr = string.split(" ");
-    
-    for (let i = 0; i < arr.length; i++) {
-        let temp = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-        capitalized.push(temp);
-    }
-
-    return capitalized.join(" ");
-
-}
-
 // Specialized function for querying Bands in Town API based on the result of an Inquirer response.
 function queryBandsInTown(response){
 
     const bandsintown = keys.bandsintown;
     // If the response is coming from the OMDB Inquirer prompt (ie, it has a 'omdbArg' key-value pair), assign 'movie' based on the returned response. Otherwise it's coming from the 'surprise' command, so assign the variable to response as is.
-    const artist = (response.bandsintownArg) ? capitalize(response.bandsintownArg) : capitalize(response);
-    const artistQuery = concat(artist);
+    const artist = (response.bandsintownArg) ? helpers.capitalize(response.bandsintownArg) : helpers.capitalize(response);
+    const artistQuery = helpers.concat(artist);
     const queryUrl = `https://rest.bandsintown.com/artists/${artistQuery}/events?app_id=${bandsintown.id}`;
 
     axios
@@ -79,7 +33,7 @@ function queryBandsInTown(response){
             }
 
             console.log(result);
-            print(result + "\n############\n"); // Print results to log.txt.
+            helpers.print(result + "\n############\n"); // Print results to log.txt.
 
         })
         .catch(error => {
@@ -95,8 +49,8 @@ function queryOMDB(response) {
 
     const omdb = keys.omdb;
     // If the response is coming from the OMDB Inquirer prompt (ie, it has a 'omdbArg' key-value pair), assign 'movie' based on the returned response. Otherwise it's coming from the 'surprise' command, so assign the variable to the response as is.
-    const movie = (response.omdbArg) ? capitalize(response.omdbArg) : capitalize(response);
-    const movieURL = concat(movie);
+    const movie = (response.omdbArg) ? helpers.capitalize(response.omdbArg) : helpers.capitalize(response);
+    const movieURL = helpers.concat(movie);
     const queryUrl = `http://www.omdbapi.com/?t=${movieURL}&y=&plot=short&apikey=${omdb.id}`;
     
     axios
@@ -110,7 +64,7 @@ function queryOMDB(response) {
             let result = `\nTitle: ${response.data.Title}\nReleased: ${response.data.Released}\nIMDB Rating: ${response.data.Ratings[0].Value}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nCast: ${response.data.Actors}\n`;
 
             console.log(result);
-            print(result + "\n############\n"); // Print results to log.txt.
+            helpers.print(result + "\n############\n"); // Print results to log.txt.
 
         })
         .catch(error => {
@@ -124,7 +78,7 @@ function querySpotify(response) {
 
     const spotify = new nodeSpotify({ id: keys.spotify.id, secret: keys.spotify.secret });
     // If the response is coming from the Spotify Inquirer prompt (ie, it has a 'spotiftyArg' key-value pair), assign 'song' based on the returned response. Otherwise it's coming from the 'surprise' command, so assign the variable to the response is.
-    const song = (response.spotifyArg) ? capitalize(response.spotifyArg) : capitalize(response);
+    const song = (response.spotifyArg) ? helpers.capitalize(response.spotifyArg) : helpers.capitalize(response);
 
     spotify.search({ type: 'track', query: song }, function(error, data) {
 
@@ -140,7 +94,7 @@ function querySpotify(response) {
         }
 
         console.log(result);
-        print(result + "\n############\n"); // Print results to log.txt.
+        helpers.print(result + "\n############\n"); // Print results to log.txt.
 
     });
 
@@ -215,7 +169,7 @@ inquirer.prompt([
                     type: "input",
                     name: "spotifyArg",
                     message: "What song do you want to learn more about?",
-                    validate: validateThis
+                    validate: helpers.validateThis
                 }
             ]).then(response => {
                 querySpotify(response);
@@ -231,7 +185,7 @@ inquirer.prompt([
                     type: "input",
                     name: "omdbArg",
                     message: "What movie do you want to learn more about?",
-                    validate: validateThis
+                    validate: helpers.validateThis
                 }
             ]).then(response => {
                 queryOMDB(response);
@@ -247,7 +201,7 @@ inquirer.prompt([
                     type: "input",
                     name: "bandsintownArg",
                     message: "Which band or musical artist's upcoming tour dates do you want to see?",
-                    validate: validateThis
+                    validate: helpers.validateThis
                 }
             ]).then(response => {
                 queryBandsInTown(response);
